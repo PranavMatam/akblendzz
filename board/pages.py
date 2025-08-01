9e67aa65e7c09733573ec4e7e29f022e639ae52a
Pranav Ravikoti Matam <25ravpr@goodnews.vic.edu.au>
2025-08-02 00:27:42 +1000

    Update: set up login and sign up pages with all visuals and links to each other

--- board/pages.py
+++ board/pages.py
@@ -3,6 +3,15 @@ from flask import Blueprint, render_template
 bp = Blueprint('pages', __name__)
 
 @bp.route('/')
+@bp.route('/login')
+def login():
+    return render_template('pages/login.html')
+
+@bp.route('/sign_up')
+def sign_up():
+    return render_template('pages/sign_up.html')
+
+@bp.route('/home')
 def home():
     return render_template('pages/home.html')
 
