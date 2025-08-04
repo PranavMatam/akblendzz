25e89d6281ec9db755be59b45ac3b4b556fc2494
Pranav Ravikoti Matam <25ravpr@goodnews.vic.edu.au>
2025-08-04 19:06:09 +1000

    Update: made rest of web pages for akshat

--- board/pages.py
+++ board/pages.py
@@ -15,3 +15,24 @@ def sign_up():
 @bp.route('/about')
 def about():
     return render_template('pages/about.html')
+    
+@bp.route('/gallery')
+def gallery():
+    return render_template('pages/gallery.html')
+
+@bp.route('/booking')
+def booking():
+    return render_template('pages/booking.html')
+
+@bp.route('/clients')
+def clients():
+    return render_template('pages/clients.html')
+
+@bp.route('/timetable')
+def timetable():
+    return render_template('pages/timetable.html')
+
+@bp.route('/finance_report')
+def finance_report():
+    return render_template('pages/finance_report.html')
+
